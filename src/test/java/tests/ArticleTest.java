070738e6547617d73220704a27db7adb6e2196f8
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-07-25 17:03:03 +0300

    mobile

--- src/test/java/tests/ArticleTest.java
+++ src/test/java/tests/ArticleTest.java
@@ -1,16 +1,19 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.MainPage;
 import pages.SearchPage;
 import static io.qameta.allure.Allure.step;
 
+@DisplayName("Wikipedia тесты")
 public class ArticleTest extends TestBase {
 
     MainPage mainPage = new MainPage();
     SearchPage searchPage = new SearchPage();
 
     @Test
+    @DisplayName("Тест-кейс №2: Проверка открытия статьи")
     void openArticleTest() {
         step("Открыть поиск и ввести запрос", () -> {
             mainPage.clickSearchIcon();
--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,15 +1,19 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.MainPage;
 import pages.SearchPage;
 import static io.qameta.allure.Allure.step;
+
+@DisplayName("Wikipedia тесты")
 public class SearchTest extends TestBase {
 
     MainPage mainPage = new MainPage();
     SearchPage searchPage = new SearchPage();
 
     @Test
+    @DisplayName("Тест-кейс №1: Проверка получения результата поиска")
     void successfulSearchTest() {
         step("Открыть поиск и ввести запрос", () -> {
             mainPage.clickSearchIcon();
